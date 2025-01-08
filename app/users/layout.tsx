8144728b165da4677875bcc96b5319f72993f7f8
Joy Brar <joybrar2001@gmail.com>
2025-01-08 09:54:13 +0530

    Users List and User Sidebar done

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,4 +1,6 @@
+import getUsers from "@/actions/getUsers";
 import Sidebar from "@/components/sidebar/Sidebar";
+import UserList from "@/components/users/UserList";
 import React from "react";
 
 export default async function UsersPageLayout({
@@ -6,9 +8,12 @@ export default async function UsersPageLayout({
 }: {
   children: React.ReactNode
 }) {
+  const users = await getUsers();
+
   return (
     <Sidebar>
       <div className="h-full">
+        <UserList items={users} />
         {children}
       </div>
     </Sidebar>
---
+++ components/users/UserBox.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+import { User } from "@prisma/client";
+import Avatar from "@/components/Avatar";
+
+interface UserBoxProps {
+  data: User;
+};
+
+export default function UserBox({ data }: UserBoxProps) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleClick = useCallback(() => {
+    setIsLoading(true);
+
+    axios
+      .post("/api/conversations", {
+        userId: data.id,
+      })
+      .then((data) => {
+        router.push(`/conversations/${data.data.id}`);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [data, router]);
+
+  return (
+    <div
+      className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
+      onClick={handleClick}
+    >
+      <Avatar user={data} />
+
+      <div className="min-w-0 flex-1">
+        <div className="focus:outline-none">
+          <div className="flex justify-between items-center mb-1">
+            <p className="text-sm font-medium text-gray-900">
+              {data.name}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
