0597abdadc924f74186147b3dd3ccf13870c1d82
Joy Brar <joy@cerebrent.com>
2025-01-15 00:00:16 +0530

    Testing

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  return NextResponse.json({ message: "hello" });
+  return NextResponse.json({ message: "Hello, this is a testing rotue" });
 }
